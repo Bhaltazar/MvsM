49757090995c5773d4759b1b68cb1d8cfea8ad3a
Bhaltazar <251271036@alumnos.utn.edu.mx>
2026-07-19 19:40:13 -0600

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,44 @@
+const CACHE_NAME = "sorpresa-cine-v1";
+const ASSETS = [
+  "./sorpresa-cine.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "./boleto.jpg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      // cachea lo que exista; si boleto.jpg no está aún, no truena la instalación
+      return Promise.all(
+        ASSETS.map((url) => cache.add(url).catch(() => {}))
+      );
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      return (
+        cached ||
+        fetch(event.request).then((res) => {
+          const resClone = res.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, resClone));
+          return res;
+        }).catch(() => cached)
+      );
+    })
+  );
+});
